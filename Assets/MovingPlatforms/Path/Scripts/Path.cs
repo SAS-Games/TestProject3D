c4752d09c36d9021a1229be2338b89471082cdc4
Abhi86shek <sonkar.abhishek86@gmail.com>
2024-08-02 19:31:35 +0530

    moving platform

---
+++ Assets/MovingPlatforms/Path/Scripts/Path.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SAS.Waypoints
+{
+    interface IPath
+    {
+        IEnumerator<Vector3> GetPathEnumerator();
+    }
+
+    public abstract class Path : IPath
+    {
+        protected List<Vector3> _points;
+        protected bool _isClosed;
+
+        public Path(List<Vector3> points, bool isClosed)
+        {
+            _points = points;
+            _isClosed = isClosed;
+        }
+
+        public abstract IEnumerator<Vector3> GetPathEnumerator();
+    }
+}
