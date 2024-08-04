e8cabd4c41d2c26fcb9e3dfb20b7347d166910b3
Abhi86Shek <sonkar.abhishek86@gmail.com>
2024-08-04 18:37:43 +0530

    platform

---
+++ Assets/MovingPlatforms/OnTriggerHandler.cs
@@ -0,0 +1,38 @@
+using System.Linq;
+using UnityEngine;
+
+public class OnTriggerHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject m_MessageListener;
+    [SerializeField] private string m_OnEnterMessage;
+    [SerializeField] private string m_OnExitMessage;
+    [SerializeField] private string[] m_CollisionTags = { "Player" };
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (string.IsNullOrEmpty(m_OnEnterMessage))
+            return;
+
+        if (m_CollisionTags.Contains(other.tag))
+        {
+            if (!m_MessageListener)
+                m_MessageListener = gameObject;
+            m_MessageListener?.SendMessage(m_OnEnterMessage, other, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log(other.name);
+        if (string.IsNullOrEmpty(m_OnExitMessage))
+            return;
+
+        if (m_CollisionTags.Contains(other.tag))
+        {
+           // Debug.Break();
+            if (!m_MessageListener)
+                m_MessageListener = gameObject;
+            m_MessageListener?.SendMessage(m_OnExitMessage, other, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
